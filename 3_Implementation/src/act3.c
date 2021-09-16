0a44fa7fbad56700df14c74808bf34ddab237b00
vaishnukulkarni <89645779+vaishnukulkarni@users.noreply.github.com>
2021-09-16 20:32:52 +0530

    Update act3.c

--- 3_Implementation/src/act3.c
+++ 3_Implementation/src/act3.c
@@ -17,76 +17,77 @@ void TimerWaveGenMode(void){
 
 int outputbyPWM(uint16_t ADC_val){
     int i;
-    char temperature[10];
+    char temper[10];
 
     if(ADC_val<=209){
 
         OCR1A = 205; //20% duty cycle
-        temperature[0] = '2';
-        temperature[1] = '0';
-        temperature[2] = 176;
-        temperature[3] = 'C';
-        temperature[4] = ' ';
-        temperature[5] = '\0';
+        temper[0] = '2';
+        temper[1] = '0';
+        temper[2] = 176;
+        temper[3] = 'C';
+        temper[4] = ' ';
+        temper[5] = '\0';
         for(i=0;i<=5;i++){
-            USARTWrite(temperature[i]);
+            USARTWrite(temper[i]);
         }
         _delay_ms(20);
     }
     else if((ADC_val>=210) && (ADC_val<=509)){
 
         OCR1A = 410; //40% duty cycle
-        temperature[0] = '2';
-        temperature[1] = '5';
-        temperature[2] = 176;
-        temperature[3] = 'C';
-        temperature[4] = ' ';
-        temperature[5] = '\0';
+        temper[0] = '2';
+        temper[1] = '5';
+        temper[2] = 176;
+        temper[3] = 'C';
+        temper[4] = ' ';
+        temper[5] = '\0';
         for(i=0;i<5;i++){
-            USARTWrite(temperature[i]);
+            USARTWrite(temper[i]);
         }
         _delay_ms(20);
     }
     else if((ADC_val>=510) && (ADC_val<=709)){
 
         OCR1A = 717;//70% duty cycle
-        temperature[0] = '2';
-        temperature[1] = '9';
-        temperature[2] = 176;
-        temperature[3] = 'C';
-        temperature[4] = ' ';
-        temperature[5] = '\0';
+        temper[0] = '2';
+        temper[1] = '9';
+        temper[2] = 176;
+        temper[3] = 'C';
+        temper[4] = ' ';
+        temper[5] = '\0';
         for(i=0;i<=5;i++){
-            USARTWrite(temperature[i]);
+            USARTWrite(temper[i]);
         }
         _delay_ms(20);
     }
     else if((ADC_val>=710) && (ADC_val<=1024)){
 
         OCR1A = 973; //95% duty cycle
-        temperature[0] = '3';
-        temperature[1] = '3';
-        temperature[2] = 176;
-        temperature[3] = 'C';
-        temperature[4] = ' ';
-        temperature[5] = '\0';
+        temper[0] = '3';
+        temper[1] = '3';
+        temper[2] = 176;
+        temper[3] = 'C';
+        temper[4] = ' ';
+        temper[5] = '\0';
         for(i=0;i<=5;i++){
-            USARTWrite(temperature[i]);
+            USARTWrite(temper[i]);
         }
         _delay_ms(20);
     }
     else{
         OCR1A = 0; //0% output
-        temperature[0] = 'O';
-        temperature[1] = 'F';
-        temperature[2] = 'F';
-        temperature[3] = ' ';
-        temperature[4] = ' ';
-        temperature[5] = '\0';
+        temper[0] = 'O';
+        temper[1] = 'F';
+        temper[2] = 'F';
+        temper[3] = ' ';
+        temper[4] = ' ';
+        temper[5] = '\0';
         for(i=0;i<=5;i++){
-            USARTWrite(temperature[i]);
+            USARTWrite(temper[i]);
         }
         _delay_ms(20);
     }
 
 }
+
